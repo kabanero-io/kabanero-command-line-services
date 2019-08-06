fbb3e25a4080b47a9bae47821fc0ac193e2f85d6
dacohen@us.ibm.com <dacohen@us.ibm.com@192.168.43.149>
2019-08-06 10:27:00 -0400

    add crud methods

--- KabCollections/src/main/java/application/rest/CollectionsAccess.java
+++ KabCollections/src/main/java/application/rest/CollectionsAccess.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,7 +26,9 @@ public class CollectionsAccess {
     @Path("/collections")
     public Response collections(@Context final HttpServletRequest request) {
 		//List collections=null;
-		// access git hub and read index.yaml (as per Ian Partridge and format response
+		// access git hub and read index.yaml (as per Ian Partridge and format response)
+		// POJO to translate yaml to List
+		String gitResponse = accessGitHub("listCollections",null);
 		JSONObject msg = new JSONObject();
 		msg.put("collections","collections");
 		return Response.ok(msg).build();
@@ -35,9 +41,77 @@ public class CollectionsAccess {
                              @PathParam("colllectionid") final String colllectionid) {
 		JSONObject collection=null;
 		// access git hub and read collection.yaml (as per Ian Partridge and format response
+		String gitResponse = accessGitHub("listCollection",colllectionid);
+		// POJO to format collection.yaml to List
 		JSONObject msg = new JSONObject();
 		msg.put("collection","collection");
 		return Response.ok(msg).build();
 
+	}
+	
+	@PUT
+    @Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Path("/collections/{colllectionid}/activate")
+    public Response activateCollection(@Context final HttpServletRequest request,
+                             @PathParam("colllectionid") final String colllectionid) {
+		// kube call to activate collection
+		String response="";
+		JSONObject msg = new JSONObject();
+		msg.put("response",response);
+		return Response.ok(msg).build();
+
+	}
+	
+	@PUT
+    @Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Path("/collections/{colllectionid}/deactivate")
+    public Response deActivateCollection(@Context final HttpServletRequest request,
+                             @PathParam("colllectionid") final String colllectionid) {
+		// kube call to deactivate collection
+		String response="";
+		JSONObject msg = new JSONObject();
+		msg.put("response",response);
+		return Response.ok(msg).build();
+
+	}
+	
+	@POST
+    @Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Path("/collections/{colllectionid}")
+    public Response addCollection(@Context final HttpServletRequest request,
+                             @PathParam("colllectionid") final String colllectionid) {
+		// kube call to add collection
+		String response="";
+		JSONObject msg = new JSONObject();
+		msg.put("response",response);
+		return Response.ok(msg).build();
+
+	}
+	
+	@DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Path("/collections/{colllectionid}")
+    public Response removeCollection(@Context final HttpServletRequest request,
+                             @PathParam("colllectionid") final String colllectionid) {
+		// kube call to delete collection
+		String response="";
+		JSONObject msg = new JSONObject();
+		msg.put("response",response);
+		return Response.ok(msg).build();
+
+	}
+	
+	private String issueKubeCommand(String cmd) {
+		String kubeResponse="";
+		return kubeResponse;
+	}
+	
+	private String accessGitHub(String func, String identifier) {
+		String gitResponse="";
+		return gitResponse;
 	}
 }
