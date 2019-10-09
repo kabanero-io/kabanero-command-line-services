76a83f8266a42166103facd15b3d91a3a66cf9ea
dacohen@us.ibm.com <dacohen@us.ibm.com@9.12.77.136>
2019-10-09 17:50:07 -0400

    move endpoint

--- src/main/java/application/HealthEndpoint.java
+++ src/main/java/application/HealthEndpoint.java
@@ -6,40 +6,18 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.ibm.json.java.JSONArray;
-import com.ibm.json.java.JSONObject;
-
-import application.rest.CollectionsUtils;
-import kabasec.HttpUtils;
-
-
+@Path("health")
 public class HealthEndpoint {
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/health")
-	public Response healthcheck() {
-		boolean healthy = true;
-		if (CollectionsUtils.readGitSuccess && HttpUtils.accessGitSuccess) {
-			healthy = true;
-		} else {
-			healthy = false;
-		}
-
-		JSONObject msg = new JSONObject();
-
-		JSONArray checks = new JSONArray();
-		checks.add("GIT");
-		msg.put("checks", checks);
-
-		if (!healthy) {
-			msg.put("status", "DOWN");
-			return Response.status(503).entity(msg).build();
-		} else {
-			msg.put("status", "UP");
-			return Response.ok(msg).build();
-		}
-
-	}
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response healthcheck() {
+      /*
+      if (!healthy) {
+        return Response.status(503).entity("{\"status\":\"DOWN\"}").build();
+      }
+      */
+      return Response.ok("{\"status\":\"UP\"}").build();
+    }
 
 }
---
+++ src/main/java/application/rest/Liveliness.java
@@ -0,0 +1,45 @@
+package application.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.ibm.json.java.JSONArray;
+import com.ibm.json.java.JSONObject;
+
+import application.rest.CollectionsUtils;
+import kabasec.HttpUtils;
+
+
+public class Liveliness {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/liveliness")
+	public Response healthcheck() {
+		boolean healthy = true;
+		if (CollectionsUtils.readGitSuccess && HttpUtils.accessGitSuccess) {
+			healthy = true;
+		} else {
+			healthy = false;
+		}
+
+		JSONObject msg = new JSONObject();
+
+		JSONArray checks = new JSONArray();
+		checks.add("GIT");
+		msg.put("checks", checks);
+
+		if (!healthy) {
+			msg.put("status", "DOWN");
+			return Response.status(503).entity(msg).build();
+		} else {
+			msg.put("status", "UP");
+			return Response.ok(msg).build();
+		}
+
+	}
+
+}
