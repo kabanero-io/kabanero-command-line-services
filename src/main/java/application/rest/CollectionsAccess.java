56979d7c9946e7517f50f45dce2c3efa960c62cf
dacohen@us.ibm.com <dacohen@us.ibm.com@9.12.76.176>
2019-11-01 10:19:21 -0400

    clean up imports

--- src/main/java/application/rest/CollectionsAccess.java
+++ src/main/java/application/rest/CollectionsAccess.java
@@ -18,8 +18,12 @@
  */
 package application.rest;
 
-import java.util.*; 
-import java.lang.*; 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
