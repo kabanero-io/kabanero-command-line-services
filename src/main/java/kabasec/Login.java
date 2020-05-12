fd713ccffe6a3d2a2f6ea5a57a5d0cb6fad6c835
davco01a <dacohen@us.ibm.com>
2020-05-12 17:07:00 -0400

    simplify

--- src/main/java/kabasec/Login.java
+++ src/main/java/kabasec/Login.java
@@ -93,18 +93,6 @@ public class Login {
             Authentication auth = new Authentication();
             checkTeamsAndGithubURL(auth);
             jwt = auth.getJwt(creds);  // check id, password/PAT, and team membership here.
-        } catch (KabaneroSecurityException e) {
-        	// Encountered an error requesting, parsing, or processing GitHub data for user
-        	if (checkApiUrlException(e)) {
-        		msg = "login failed, you may want to check your authorization configuration. Double check the apiUrl: in your github: configuraton in the Kabanero CR Instance to make sure it's correct";
-        		System.out.println("An error occurred during authentication for user, double check the apiUrl: in your github: configuraton in the Kabanero CR Instance to make sure it's correct, exception message: "+ e.getMessage());
-        		Properties p = new Properties();
-                p.put("message", msg);
-                return p;
-        	} else {
-        		System.out.println("An error occurred during authentication for user, exception message: "+ e.getMessage());
-        	}
-        	return returnError(e.getStatusCode(), "An error occurred during authentication for user ", e);
         } catch (Exception e) {
         	if (checkApiUrlException(e)) {
         		msg = "login failed, you may want to check your authorization configuration. Double check the apiUrl: in your github: configuraton in the Kabanero CR Instance to make sure it's correct";
